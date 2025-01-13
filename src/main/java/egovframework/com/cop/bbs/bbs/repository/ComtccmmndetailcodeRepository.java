ee9f9f4f910f1b40bc250b23014b89f76567dc09
yongfire38 <yongfire38@gmail.com>
2025-01-13 16:06:48 +0900

    Fix: 타입 오류 수정

--- src/main/java/egovframework/com/cop/bbs/bbs/repository/ComtccmmndetailcodeRepository.java
+++ src/main/java/egovframework/com/cop/bbs/bbs/repository/ComtccmmndetailcodeRepository.java
@@ -1,13 +1,15 @@
 package egovframework.com.cop.bbs.bbs.repository;
 
 import egovframework.com.cop.bbs.bbs.entity.Comtccmmndetailcode;
+import egovframework.com.cop.bbs.bbs.entity.ComtccmmndetailcodeId;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface ComtccmmndetailcodeRepository extends JpaRepository<Comtccmmndetailcode, String> {
+public interface ComtccmmndetailcodeRepository extends JpaRepository<Comtccmmndetailcode, ComtccmmndetailcodeId> {
 
     List<Comtccmmndetailcode> findByUseAtAndComtccmmndetailcodeId_CodeId(String useAt, String codeId);
 }
